0530b2625f4de8ff62156b7d50e04cbc1371c648
daniel kyman <danny.kyman@gmail.com>
2020-03-17 15:05:22 -0700

    completed mvp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { Provider } from "react-redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import { createStore } from "redux";
+
+import { reducer } from "./reducers/reducer";
+
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
